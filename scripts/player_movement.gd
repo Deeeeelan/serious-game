1cd92d34de05f5af9a8c906f84e0b629936ccf99
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-06-21 16:11:59 -0700

    Add tile carrying

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var ground_tm: TileMapLayer
 @export var underground_tm: TileMapLayer
 
+@export var carrying := false
+var carrying_data
+
 const SPEED = 320.0
 const LERP_SPEED = 0.08
 
@@ -16,10 +19,21 @@ func cell_pos_to_texture(tm: TileMapLayer, tm_pos: Vector2i) -> Texture:
 	return ImageTexture.create_from_image(t_img)
 
 func _input(event: InputEvent) -> void:
-	if event.is_action("interact"):
+	if event.is_action_pressed("interact"):
 		var tm_pos = ground_tm.local_to_map(position)
-		if ground_tm.get_cell_source_id(tm_pos) != -1:
-			$Holding.texture = cell_pos_to_texture(ground_tm, tm_pos)
+		if not carrying:
+			if ground_tm.get_cell_source_id(tm_pos) != -1:
+				carrying = true
+				carrying_data = ground_tm.get_cell_atlas_coords(tm_pos)
+				$Holding.texture = cell_pos_to_texture(ground_tm, tm_pos)
+				ground_tm.set_cell(tm_pos, -1)
+		else:
+			if ground_tm.get_cell_source_id(tm_pos) == -1:
+				carrying = false
+				$Holding.texture = null
+				ground_tm.set_cell(tm_pos, 0, carrying_data)
+				
+		
 
 func _physics_process(delta: float) -> void:
 
