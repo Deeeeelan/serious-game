7e20e7d95e4a14514ef9da958838a8d9c4a3e44c
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-06-21 16:46:45 -0700

    Add carry buffer

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -19,6 +19,15 @@ func cell_pos_to_texture(tm: TileMapLayer, tm_pos: Vector2i) -> Texture:
 	var t_img := img.get_region(rect)
 	return ImageTexture.create_from_image(t_img)
 
+func carry_at_pos(pos: Vector2i):
+	if ground_tm.get_cell_source_id(pos) != -1:
+		carrying = true
+		carrying_data = ground_tm.get_cell_atlas_coords(pos)
+		var img = cell_pos_to_texture(ground_tm, pos)
+		$Holding.texture = img
+		$Aim.texture = img
+		ground_tm.set_cell(pos, -1)
+
 func drop_at_pos(pos: Vector2i):
 	if ground_tm.get_cell_source_id(pos) == -1:
 		carrying = false
@@ -28,17 +37,11 @@ func drop_at_pos(pos: Vector2i):
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
-		var tm_pos = ground_tm.local_to_map(position)
-		var thrw_pos = ground_tm.local_to_map(position + get_local_mouse_position())
 		if not carrying:
-			if ground_tm.get_cell_source_id(tm_pos) != -1:
-				carrying = true
-				carrying_data = ground_tm.get_cell_atlas_coords(tm_pos)
-				var img = cell_pos_to_texture(ground_tm, tm_pos)
-				$Holding.texture = img
-				$Aim.texture = img
-				ground_tm.set_cell(tm_pos, -1)
+			var tm_pos = ground_tm.local_to_map(position)
+			carry_at_pos(tm_pos)
 		else:
+			var thrw_pos = ground_tm.local_to_map(position + get_local_mouse_position())
 			drop_at_pos(thrw_pos)
 	elif event.is_action_pressed("drop"):
 		if carrying:
@@ -49,6 +52,12 @@ func _process(delta: float) -> void:
 	$Aim.position = get_local_mouse_position()
 
 func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("interact") and not carrying:
+		var tm_pos = ground_tm.local_to_map(position)
+		carry_at_pos(tm_pos)
+		
+	
 	var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
 	velocity = lerp(velocity, direction * SPEED, LERP_SPEED)
 
