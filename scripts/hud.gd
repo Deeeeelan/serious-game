f37cbf1ed4611384ae5af10d55af32bd60a7082b
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-06-23 14:41:11 -0700

    Add HUD

---
+++ scripts/hud.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _process(delta: float) -> void:
+	$VBoxContainer/RichTextLabel.text =\
+"[b]Gold:[/b] %s
+[b]Wood:[/b] %s
+[b]Stone:[/b] %s
+[b]Iron:[/b] %s" % [str(GameStats.gold), str(GameStats.wood), str(GameStats.stone), str(GameStats.iron)]
--- scripts/tiles/game_stats.gd
+++ scripts/tiles/game_stats.gd
@@ -6,3 +6,4 @@ var time: int = 0
 var wood: int = 0
 var stone: int = 0
 var gold: int = 0
+var iron: int = 0
