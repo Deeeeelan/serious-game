f399617740c9e013412514aa067c27a54b5d33ea
FrankT1 <sixingt123@gmail.com>
2026-06-22 17:54:25 -0700

    Update gear.gd

--- scripts/gear.gd
+++ scripts/gear.gd
@@ -1,33 +1,46 @@
 extends Node2D
 
 var components: Dictionary[Vector2i,Component] = {}
-var gears: Dictionary = {}
-var generators: Dictionary = {}
+var generators: Array[Vector2i] = []
 var genInfo: Dictionary = {1:{"speed":10, "torque":50}, 2:{"speed":20, "torque":100}}
+
+##References to other objects
 @onready var player: CharacterBody2D = $"../Node2D/Player"
 @onready var underground: TileMapLayer = $"../Node2D/Terrain/Underground"
 
 class Component:
-	var size: int = 32
-	var speed: int = 0
-	var torque: int = 0
-	var isGenerator: bool = false
-	
-	func _init(siz, spee, torqu, isGenerato) -> void:
+	var size: int
+	var speed: int
+	var torque: int
+	var firstCheck: bool
+	var genID: int
+
+	func _init(siz = 32, spee = 0, torqu = 0, genI = 0) -> void:
 		self.size = siz
 		self.speed = spee
 		self.torque = torqu
-		self.isGenerator = isGenerato
+		self.genID = genI
+		if genID == 0:
+			firstCheck == true
+
+	func resetTo(siz = 32, spee = 0, torqu = 0, genI = 0) -> void:
+		self.size = siz
+		self.speed = spee
+		self.torque = torqu
+		self.genID = genI
+		if genID == 0:
+			firstCheck == true
+
 
 @warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
 
-func findConnectedComponents(startPoint: Vector2i) -> Array:
+func findConnectedComponents(startPoint: Vector2i) -> Array: #TODO: make this better/integrate this better
 	var componentStack: Array[Vector2i] = [startPoint]
 	var index: int = 0
 	
-	while componentStack.size() > index:
+	while componentStack.size() >= index:
 		var node: Vector2i = componentStack.pop_front()
 
 		if components.has(node + Vector2i.UP) and (node + Vector2i.UP) not in componentStack:
@@ -39,35 +52,47 @@ func findConnectedComponents(startPoint: Vector2i) -> Array:
 		if components.has(node + Vector2i.LEFT) and (node + Vector2i.UP) not in componentStack:
 			componentStack.append(node + Vector2i.LEFT)
 		
-		print(componentStack)
 		index += 1
 	return componentStack
 
 func _input(event: InputEvent) -> void:
 	var playerTilemapCoords: Vector2i = underground.local_to_map(player.position)
 
-	if event.is_action_pressed("placeGear"):
-		print(playerTilemapCoords, "placed Gear")
-		#components[playerTilemapCoords] = 0
-		gears[playerTilemapCoords] = [0, 0, false]
+	if event.is_action_pressed("placeGear"): ##testing version of placing gear
+		if playerTilemapCoords in generators:
+			generators.erase(playerTilemapCoords)
+			components[playerTilemapCoords].resetTo()
+		elif playerTilemapCoords in components:
+			components[playerTilemapCoords].resetTo()
+		else:
+			components[playerTilemapCoords] = Component.new()
 		updateGearRendering()
-	if event.is_action_pressed("placeTestGen"):
-		print(playerTilemapCoords, "placed Gen")
-		#components[playerTilemapCoords] = 1
+	
+	if event.is_action_pressed("placeTestGen"):##testing version of placing generator
+		if playerTilemapCoords in components:
+			components[playerTilemapCoords].resetTo(32, 10, 50, 1)
+			generators.append(playerTilemapCoords)
+		else:
+			components[playerTilemapCoords] = Component.new(32, 10, 50, 1)
+			generators.append(playerTilemapCoords)
 		
 		updateGearRendering()
 		#updateGearLogic()
 
 func updateGearRendering() -> void:
-	for gearCoords in gears:
-		underground.set_cell(gearCoords, 0, Vector2i(0, 0))
+	for componentCoords in components:
+		var component: Component = components[componentCoords]
+		underground.set_cell(componentCoords, 0, Vector2i(0, component.genID))
 		
-		if gears.has(gearCoords) and gears[gearCoords][2] == true:
-			underground.set_cell(gearCoords, 0, Vector2i(1, 0))
+		if component.genID == 0 and component.speed != 0:
+			underground.set_cell(componentCoords, 0, Vector2i(1, 0))
 
-#func updateGearLogic() -> void:
+#func updateGearLogic() -> void: ##TODO: This
 	#for componentCoords in components:
 		#var component = components[componentCoords]
 #
 		#for coords in findConnectedComponents(componentCoords):
 			#gears[coords] = [genInfo[0], genInfo[1], true]
+
+func updateGearLogic() -> void:
+	pass
